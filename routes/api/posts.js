c3bf63d130f91b3b17de1cf7c07b74f93993a12d
getMaxed <maxfaden07@gmail.com>
2019-01-15 15:41:23 -0800

    Create Post model. Add posts route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
 /*
 |--------------------------------------------------------------------------
-| @route    GET api/posts/test
-| @desc     Tests post route
-| @access   Public
+| LOAD POST MODEL
 |--------------------------------------------------------------------------
 */
 
-router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }));
+const Post = require('../../models/Post');
+
+/*
+|--------------------------------------------------------------------------
+| LOAD VALIDATION
+|--------------------------------------------------------------------------
+*/
+
+const validatePostInput = require('../../validation/post');
+
+/*
+|--------------------------------------------------------------------------
+| @route    POST api/posts/posts
+| @desc     Create post
+| @access   Private
+|--------------------------------------------------------------------------
+*/
+
+router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    (req, res) => {
+        const { errors, isValid } = validatePostInput(req.body);
+        /*
+        |--------------------------------------------------------------------------
+        | CHECK VALIDATION
+        |--------------------------------------------------------------------------
+        */
+        if (!isValid) {
+            return res.status(400).json(errors);
+        }
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: req.body.name,
+            avatar: req.body.avatar,
+            user: req.user.id
+        });
+
+        newPost.save().then(post => res.json(post));
+    }
+);
 
 module.exports = router;
